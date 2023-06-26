0bde119f45bd520237bce41e2b20994fec379969
@Akirienko <lakirienkol@gmail.com>
2023-06-26 16:31:53 +0300

    nuxt 3 practice

---
+++ components/movie/search.vue
@@ -0,0 +1,37 @@
+<script setup>
+  const query = ref("batman");
+  const movies = ref([]);
+
+  async function search() {
+    const { Search } = await $fetch(`http://www.omdbapi.com/?apikey=277a8905&s=${query.value}`)
+    movies.value = Search;
+    console.log(movies);
+  }
+
+  search()
+</script>
+
+<template>
+  <form @submit.prevent="search">
+    <input type="text" v-model="query">
+    <button>Search</button>
+  </form>
+  <ul class="films-list">
+    <li v-for="movie in movies" :key="movie.imdbID">
+      <NuxtLink :to="{ name: 'movies-id', params: {id: movie.imdbID} }">
+        <img :src="movie.Poster" :alt="movie.Title">
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+
+
+<style scoped>
+.films-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  list-style: none;
+}
+</style>
---
+++ pages/movies/[id].vue
@@ -0,0 +1,38 @@
+
+<script setup>
+  const route = useRoute();
+  const {data} = await useFetch(
+    `http://www.omdbapi.com/?apikey=277a8905&i=${route.params.id}`,
+    {
+      pick: ["Plot", "Title", "Error"],
+      key: `/movie/${route.params.id}`,
+
+    }
+  );
+
+  if(data.value.Error === "Incorrect IMDb ID.") {
+    showError({statusCode: 404, statusMessage: "page Not Found"});
+  }
+
+  
+  // const { data } = useAsyncData(()=> {
+  //   return $fetch(`http://www.omdbapi.com/?apikey=277a8905&i=${route.params.id}`)
+  // },
+  // {
+  //   pick: ["Plot", "Title"],
+  //   // transform(data) {
+  //   //   return {
+  //   //     Plot: data.Plot,
+  //   //     Title: data.Title
+  //   //   }
+  //   //   // return data.Title;
+  //   // }
+  // }
+// );
+
+</script>
+<template>
+  <div>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
